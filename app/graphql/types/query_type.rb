4690931586e17aca15b9b17f2bd0ed9e39f45b31
Steven Torrence <storrence88@gmail.com>
2019-07-29 21:52:05 -0500

    Fix typo in the query_type.rb method

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
     field :items,
-         [Types::ItemType],
-         null: false,
-         description: 'Returns a list of items in the martian library'
+          [Types::ItemType],
+          null: false,
+          description: "Returns a list of items in the martian library"
 
-    def item
+    def items
       Item.all
     end
   end
