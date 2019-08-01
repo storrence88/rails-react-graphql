c2e05e5814da0983023bf1b525cd960f75f60998
Steven Torrence <storrence88@gmail.com>
2019-07-31 21:21:23 -0500

    Create Library component and include in the main Provider component

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
+import Library from '../components/Library';
 
-render(<Provider>👻</Provider>, document.querySelector('#root'));
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+);
