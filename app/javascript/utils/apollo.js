6139a052508efeac0743a285788b56a09a5c4788
Steven Torrence <storrence88@gmail.com>
2019-07-31 20:44:53 -0500

    Add ability to grab CSRF token from a Rails POST request

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -13,3 +13,35 @@ export const createCache = () => {
   }
   return cache;
 };
+
+// getToken from meta tags
+const getToken = () =>
+  document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const token = getToken();
+const setTokenForOperation = async operation =>
+  operation.setContext({
+    headers: {
+      'X-CSRF-Token': token,
+    },
+  });
+// link with token
+const createLinkWithToken = () =>
+  new ApolloLink(
+    (operation, forward) =>
+      new Observable(observer => {
+        let handle;
+        Promise.resolve(operation)
+          .then(setTokenForOperation)
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
